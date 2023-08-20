45abed0d4cfcd5650ced19ba4f531fb6bd69d447
cetaceainthesky <syoh5188@gmail.com>
2023-08-21 03:27:38 +0900

    heap with tree

---
+++ seoseo/heap/heap-with-array.py
@@ -0,0 +1,16 @@
+import time
+from collections import deque
+
+start = time.time()  # Record the start time
+
+def printTime(start):
+    # Calculate the runtime by subtracting the current time from the start time
+    print("Runtime:", time.time() - start)
+    print()
+
+# Implementation of the binary tree
+
+
+# End of the code
+print()
+printTime(start)
